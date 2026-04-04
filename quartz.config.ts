dc1349887db97d041e4fc0eb56150a34f0f913b1
Faeri <faerimagic@gmail.com>
2026-04-03 23:05:42 -0400

    Quartz sync: Apr 3, 2026, 11:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "faerimagic.github.io/faeri-obsidian-vaults",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
